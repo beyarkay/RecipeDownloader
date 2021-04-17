f09f8e4a0e62fdfa3d1bad9012485b8384a6eeaf
beyarkay <boydrkane@gmail.com>
2021-04-17 18:28:33 +0200

    Auto download with exception handling, csv conversion

--- download.py
+++ download.py
@@ -1,14 +1,22 @@
+import traceback
+
 import requests
 from bs4 import BeautifulSoup
 import dl_allrecipescom
 from pprint import pprint
+import pandas as pd
+import tqdm
 
 def main():
-    all_links = dl_allrecipescom.fetch_item_links(limit=10)
+    all_links = dl_allrecipescom.fetch_item_links(limit=200)
     items = []
-    for link in all_links:
-        items.append(dl_allrecipescom.link_to_dict(link))
-        pprint(items[-1])
+    for link in tqdm.tqdm(all_links):
+        try:
+            items.append(dl_allrecipescom.link_to_dict(link))
+        except Exception:
+            traceback.print_exc()
+        #pprint(items[-1])
+    pd.DataFrame(items).to_csv("recipes.csv")
 
 if __name__ == "__main__":
     main()
