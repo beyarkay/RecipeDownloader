f3178b49a5ff812bd595b60512689f5be2d86fad
beyarkay <boydrkane@gmail.com>
2021-04-19 17:33:31 +0200

    Update: save the recipes as they are being downloaded

--- download.py
+++ download.py
@@ -12,12 +12,14 @@ def main():
     all_links = dl_allrecipescom.fetch_item_links(limit=1000)
     items = []
     pbar = tqdm.tqdm(all_links)
-    for link in pbar:
+    for i, link in enumerate(pbar):
         try:
             pbar.set_description(desc=link)
             items.append(dl_allrecipescom.link_to_dict(link))
         except Exception:
             traceback.print_exc()
+        if i % 10 == 0:
+            pd.DataFrame(items).to_csv("recipes.csv")
     pd.DataFrame(items).to_csv("recipes.csv")
 
 
