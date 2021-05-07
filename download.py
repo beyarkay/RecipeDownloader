8eb1090e52c7cc2bb306b3c30c0f6c22e65a2976
beyarkay <boydrkane@gmail.com>
2021-05-07 11:30:57 +0200

    Improve memory management for large CSVs

--- download.py
+++ download.py
@@ -10,6 +10,7 @@ from pprint import pprint
 import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('display.precision', 2)
+import numpy as np
 import tqdm
 
 
@@ -24,7 +25,13 @@ def main():
         all_links = dl_allrecipescom.fetch_item_links(limit=limit)
     items = []
     if os.path.exists('recipes.csv'):
-        existing_recipes = pd.read_csv('recipes.csv')
+        print("Loading recipes.csv...")
+        col_names = pd.read_csv('recipes.csv', nrows=0).columns
+        types_dict = {col: np.float16 for col in col_names if 'uses_' in col}
+        existing_recipes = pd.read_csv('recipes.csv', dtype=types_dict)
+        print(f"Loaded recipes.csv: {round(existing_recipes.memory_usage().sum()/1024/1024)} MB")
+        existing_recipes.loc[:, existing_recipes.dtypes == np.float] = existing_recipes.loc[:, existing_recipes.dtypes == np.float].astype(np.float16)
+        print(f"Minified recipes.csv: {round(existing_recipes.memory_usage().sum()/1024/1024)} MB")
         existing_links = set(existing_recipes['link'].to_list())
         links_to_dl = list(set(all_links) - existing_links)
         print(f'Recipe links ({len(all_links)}) - existing recipes ({len(existing_recipes)}) = links to download ({len(links_to_dl)})')
@@ -42,27 +49,29 @@ def main():
             results = []
             links = []
             for _ in range(min(NUM_PROCESSES, len(links_to_dl))):
-                links.append(links_to_dl.pop())
+                links.append(links_to_dl.pop(0))
                 results.append(pool.apply_async(dl_allrecipescom.link_to_dict, (links[-1],)))
             for link, res in zip(links, results):
                 try:
                     items.append(res.get(timeout=10))
-                    pbar.set_description(desc=link)
+                    pbar.set_description(desc=f"{link} ({round(existing_recipes.memory_usage().sum()/1024/1024)}MB)")
                     pbar.update()
                 except TimeoutError as e:
                     links_to_dl.append(link)
-                    traceback.print_exc()
+                    print(f"link {link} timed out, appending to links_to_dl")
                 except Exception:
                     traceback.print_exc()
 
             if len(items) > 100:
+                pbar.set_description(desc=f"Appending and saving ({round(existing_recipes.memory_usage().sum()/1024/1024)}MB)")
                 df = pd.DataFrame(items)
-                #print(f"{len(df)} + {len(existing_recipes)} = {len(df) + len(existing_recipes)}")
+                df.loc[:, df.dtypes == np.float] = df.loc[:, df.dtypes == np.float].astype(np.float16)
                 if len(existing_recipes) == 0:
                     existing_recipes = df.copy()
                 else:
-                    existing_recipes = pd.concat([existing_recipes, df], ignore_index=True, sort=False)
+                    existing_recipes = pd.concat([existing_recipes, df], ignore_index=True, sort=False, copy=False)
                 existing_recipes.to_csv("recipes.csv")
+                pbar.set_description(desc=f"Saved: {round(existing_recipes.memory_usage().sum()/1024/1024)}MB")
                 items = []
 
     existing_recipes.append(pd.DataFrame(items))
